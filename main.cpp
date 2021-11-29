e16677c6ed9599df201a1b4f7cce9506f0ef96ed
leonardo16122002 <78797793+leonardo16122002@users.noreply.github.com>
2021-11-29 22:46:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+using namespace std;
+int n;
+int main() {
+    cin>>n;
+    //for(int i=0;i<100;i++){
+        if(primo(n)) {
+            cout << "numero primo" << endl;
+        }else{
+            cout << "numero non primo" << endl;
+        }
+    //}
+    return 0;
 }
