1ebbf4d8f10835475e414c1dc6ef1e24156090d1
leonardo16122002 <78797793+leonardo16122002@users.noreply.github.com>
2021-11-29 22:46:00 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
+//
+// Created by leona on 29/11/2021.
+//
+
+#include "lib.h"
+
+bool primo(int a){
+    int i,j,k;
+
+    i=2;
+
+    while(i<=a/2){
+        //std::cout<<"a="<<a<<"  %  "<<"i="<<i<<" = "<<a%i<<std::endl;
+        if(a%i==0){
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
 
