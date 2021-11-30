1baa4b72ae833281a94ed00038a83216da5b026a
leonardo16122002 <78797793+leonardo16122002@users.noreply.github.com>
2021-11-30 09:56:32 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -4,18 +4,15 @@
 
 #include "lib.h"
 
-bool primo(int a){
-    int i,j,k;
-
-    i=2;
-
-    while(i<=a/2){
-        //std::cout<<"a="<<a<<"  %  "<<"i="<<i<<" = "<<a%i<<std::endl;
-        if(a%i==0){
-            return false;
-        }
-        i++;
+bool ric(int i, int n){
+    
+    if(i==n){
+        return true;
     }
-    return true;
+    else if(n%i==0){
+        return false;
+    }
+    else {
+        return ric(i+1,n);
+    }
 }
-
